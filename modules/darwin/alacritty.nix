61049627b2c6b76aad0ed027e7cc0d8ad9d67ede
briheet <briheetyadav@gmail.com>
2026-02-22 22:40:57 +0530

    upd: alacritty

--- modules/darwin/alacritty.nix
+++ modules/darwin/alacritty.nix
@@ -1,9 +1,29 @@
 { pkgs, ... }:
 {
 
+  home.packages = [ pkgs.alacritty.terminfo ];
+
   programs.alacritty = {
     enable = true;
-    theme = "solarized_dark";
+    theme = "deep_space";
+
+    settings = {
+      env = {
+        TERM = "xterm-256color";
+        TERM_PROGRAM = "Alacritty";
+        SHELL = "${pkgs.fish}/bin/fish";
+      };
+
+      window = {
+        opacity = 0.7;
+        blur = true;
+      };
+
+      terminal = {
+        shell = "${pkgs.fish}/bin/fish";
+      };
+
+    };
   };
 
 }
